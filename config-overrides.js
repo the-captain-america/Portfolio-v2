e8e3a331bc375b2c7b940514fac41c6301792868
Vinu <133609222+vinu-codes@users.noreply.github.com>
2023-11-21 11:26:40 +0000

    Update config-overrides.js and package.json

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,6 @@ const path = require('path')
 module.exports = override(
   addWebpackAlias({
     ['@pages']: path.resolve(__dirname, './src/pages'),
-    ['@public']: path.resolve(__dirname, './public'),
     ['@common']: path.resolve(__dirname, './src/common'),
     ['@components']: path.resolve(__dirname, './src/components'),
     ['@utils']: path.resolve(__dirname, './src/utils'),
