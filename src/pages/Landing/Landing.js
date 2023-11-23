5306428b25317d0a90fe32b3fdd83bc141171f14
Captain America <47872483+the-captain-america@users.noreply.github.com>
2023-11-23 01:35:30 +0000

    refactor: Update LandingPage to use Tabs component
    Deleted Tab.js and Tabs index.js, imported and used Tabs component in LandingPage.

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Grid, Row } from '@common/Grid'
 
 import { Footer } from '@common/Footer'
 import Card from '@common/Card'
 import { Content } from '@common/Content'
+import { Tabs } from '@common/Tabs'
 
-const LandingPage = () => (
-  <>
-    <Content>
-      <Grid marginX={0}>
-        <Row>
-          <Card sm={12}>Stuff</Card>
-        </Row>
-      </Grid>
-    </Content>
-    <Footer />
-  </>
-)
+const options = [
+  { label: 'label 1', id: 'label1' },
+  { label: 'label 2', id: 'label2' },
+]
+
+const LandingPage = () => {
+  const [activeTab, setActiveTab] = useState('')
+
+  const handleCallback = (data) => {
+    setActiveTab(data)
+  }
+
+  return (
+    <>
+      <Content>
+        <Grid marginX={0}>
+          <Row>
+            <Card sm={12}>
+              <Tabs options={options} callback={handleCallback} />
+            </Card>
+          </Row>
+        </Grid>
+      </Content>
+      <Footer />
+    </>
+  )
+}
 
 export default LandingPage
