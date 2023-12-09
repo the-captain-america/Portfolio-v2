6ac111e386780cad87527478a8c49f1ba9c6237f
vinu-codes <vinujithmin@gmail.com>
2023-12-09 17:20:15 +1100

    changes to AboutMe

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,13 +3,25 @@ import { Content } from '@common/Content'
 import { Grid, Row } from '@common/Grid'
 import { Main } from '@common/Main'
 
+const aboutMeInfo = (
+  <p>
+    I am a frontend developer with an accounting background and a strong passion
+    for technology and coding. My main areas of expertise include HTML, CSS,
+    JavaScript, React, and Styled Components. My main goal is to build top-class
+    web applications and websites.
+  </p>
+)
+
 const AboutMePage = () => {
   return (
     <Content>
       <Grid>
         <Row>
-          <Main></Main>
-          <div>hi</div>
+          <Main
+            subtitle="A bit about me..."
+            title="About Me"
+            info={aboutMeInfo}
+          ></Main>
         </Row>
       </Grid>
     </Content>
